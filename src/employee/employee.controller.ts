bf18b57e6fe3ab9898249d10a69c5bf71ce2f65b
Siva Prakash <prakashdeveloper@outlook.in>
2024-05-04 16:25:16 +0530

    Updated controller & service

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import { CreateEmployeeDto, UpdateEmployeeDto } from './employee.dto';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  @Get()
+  async findAll() {
+    return this.employeeService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeeService.create(createEmployeeDto);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+    return this.employeeService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.employeeService.remove(id);
+  }
+}
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { MongoClient, ObjectId } from 'mongodb';
+import { CreateEmployeeDto, UpdateEmployeeDto } from './employee.dto';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  private readonly collection;
+
+  constructor() {
+    this.collection = new MongoClient('mongodb://localhost:27017/').db('hrms').collection('employees');
+  }
+
+  async findAll() {
+    const cursor = this.collection.find({});
+    return await cursor.toArray();
+  }
+
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const result = await this.collection.insertOne(createEmployeeDto);
+    return await this.collection.findOne({ _id: result.insertedId });
+  }
+
+  async update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
+    const objId = new ObjectId(id);
+    const result = await this.collection.findOneAndUpdate(
+      { _id: objId },
+      { $set: updateEmployeeDto },
+      { returnDocument: 'after' }
+    );
+    if (!result.value) {
+      throw new NotFoundException('Employee not found');
+    }
+    return result.value;
+  }
+
+  async remove(id: string) {
+    const objId = new ObjectId(id);
+    const result = await this.collection.deleteOne({ _id: objId });
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('Employee not found');
+    }
+    return { message: 'Record deleted' };
+  }
+}
